8515bf7df56a48eaeee69e79b34d852aadfbe1a6
fansan6190 <fanindra.saini@axeno.co>
2026-03-31 12:29:55 +0530

    removed spaceadder

--- blocks/space-adder/space-adder.js
+++
@@ -1,9 +0,0 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
-export default function decorate(block) {
-  // The original HTML already contains the <section class="verticalPadding_section padding-80"></section>
-  // The block itself is just a container for this section.
-  // No additional elements need to be created or appended by the JS.
-  // The block is essentially a wrapper for a pre-defined empty section.
-}
